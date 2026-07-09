66cc07198f8eb0728ed62cee864649394f524d11
jlsbollman <jsbollman@gmail.com>
2026-07-09 15:19:23 -0500

    Fix exceeded height scrolling.

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -5,8 +5,8 @@ import { SiteFooter } from "./SiteFooter";
 
 export function RootLayout() {
   return (
-    <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
-      <div style={{ flex: 1, display: "flex", flexDirection: "column" }}>
+    <div style={{ display: "flex", flexDirection: "column", height: "100vh", overflow: "hidden" }}>
+      <div style={{ flex: 1, minHeight: 0, display: "flex", flexDirection: "column" }}>
         <Outlet />
       </div>
       <SiteFooter />
